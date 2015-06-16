449d174050b481b29eeaecd02575664cfe4073ad
Andrew Trimble <andrew.trimble@archarithms.com>
2015-06-16 14:56:36 -0500

    Added more commands.

--- bigio/cli.py
+++ bigio/cli.py
@@ -17,3 +17,17 @@ def run(bigio):
         elif cmd == 'members':
             for member in bigio.cluster.member_holder.get_active_members():
                 logger.info(member)
+        elif cmd == 'listeners':
+            for key, topic_list in bigio.cluster.listener_registry.get_all_registrations().items():
+                logger.info(key + ' ->')
+                for topic in topic_list:
+                    logger.info('    ' + topic)
+        elif cmd == 'help':
+            logger.info('Commands:')
+            logger.info('members   - list all the known active members')
+            logger.info('whoami    - display information about this node')
+            logger.info('listeners - list all known registered listeners')
+            logger.info('quit      - exit the program')
+            logger.info('help      - print this message')
+        elif cmd != '':
+            logger.info('Unrecognized command "' + cmd + '" - type "help" for a list of available commands.')
